60f403b448194f83c8b899d06c6e38d20f37f9e5
Li Tao <aeolia.zafiro@gmail.com>
2021-03-17 15:05:04 +0800

    Update similarity_calculation.m

--- similarity_calculation.m
+++ similarity_calculation.m
@@ -1,9 +1,9 @@
-image1= imread('/Users/taolee/Library/Mobile Documents/com~apple~CloudDocs/Work/Figure_simularity/HTS-Encoded-03_copy.png');
+image1= imread('HTS-Encoded-03_copy.png');
 
 ref=rgb2gray(image1);
-res=cell(36,1);
+res=cell(15,1);%%15 is the total number of image2
 for N=1:15 
-    image2=imread(['/Users/taolee/Library/Mobile Documents/com~apple~CloudDocs/Work/Figure_simularity/mdc/216_',num2str(N),'.png']); 
+    image2=imread(['/mdc/216_',num2str(N),'.png']); 
     A=rgb2gray(uint8(image2));
     res{N,1}=similarity(ref,A);
 end
